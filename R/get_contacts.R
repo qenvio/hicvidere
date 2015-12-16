53bbbc45523477aa0a6ec2b4fb750874d10198a7
qenvio <enrique.vidal@crg.eu>
2015-12-16 10:24:21 +0100

    revert last commit

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -14,42 +14,15 @@ get_contacts <- function(matrix_file, region){
 
   region_coord <- gsub("^.*:", "",  region) %>% strsplit("-") %>% unlist %>% as.numeric
   chrom <- gsub(":.*$", "", region)
-
-  # get coordinates
-
-  coordinates <- strsplit(region, "[:-]") %>% unlist
-
-  if(length(coordinates) == 1){
-
-    start <- 0
-    end <- max(c(out$V2, out$V3))
-
-  }else if(length(coordinates) == 2){
-
-    start <- as.numeric(coordinates[2])
-    end <- max(c(out$V2, out$V3))
-
-  }else{
-
-    start <- as.numeric(coordinates[2])
-    end <- as.numeric(coordinates[3])
-
-  }
-
-  pos <- seq(floor(start / window + .5) * window, floor(end / window + .5) * window, window) %>%
-      format(sci = F)
-
-    
+  
   # read file
 
   contacts <- read_tabix(matrix_file, region)
-
+   
   # get only the desired square
 
   contacts %<>% filter(V2 > region_coord[1], V2 < region_coord[2],
                        V3 > region_coord[1], V3 < region_coord[2]) %>%
-      mutate(V2 = factor(V2, levels = pos),
-             V3 = factor(V3, levels = pos)) %>%
       xtabs(V4 ~ V2 + V3, .)
 
   # output
